a762a2c8f3740055880eb003a77743afe68714c3
IvanProa <138053177+IvanProa@users.noreply.github.com>
2023-11-28 05:23:48 -0300

    Create ImagenesCLP.js

---
+++ ImagenesCLP.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Función que se ejecuta cuando la página se carga completamente
+document.addEventListener("DOMContentLoaded", function () {
+    // Muestra un mensaje de alerta
+    alert("¡Bienvenido a la página de turismo en Carlos Paz!");
+});
